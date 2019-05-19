992d42ad9b99d5ead64c35a2c6df2ef919e3c35e
BalfDZC <15521301419@163.com>
2019-05-19 09:01:12 +0800

    微信签到功功能

---
+++ pages/index/index.js
@@ -0,0 +1,13 @@
+import initCalendar,{ jump }  from '../../component/calendar/main.js';
+
+const conf = {
+  data: {
+
+
+  },
+  onShow: function() {
+    initCalendar({ disablePastDay: true}); //  自定义配置 initCalendar({ multi: true, ... })
+    // jump(2018, 6, 6);
+  }
+};
+Page(conf);
